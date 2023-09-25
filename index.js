bcbe6742415c51b617bf3d5ef27f2932086ed619
j.kovalev <j.kovalev@q1.eu>
2023-09-25 16:23:11 +0200

    Add jsx-max-props-per-line and jsx-first-prop-new-line

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ module.exports = {
 	},
 	configs: {
 		recommended: {
-			extends: [
-				'eslint:recommended',
-			],
+			extends: ['eslint:recommended'],
 			rules: {
-				'no-multiple-empty-lines': ['error', {
-					'max': 1,
-					'maxEOF': 0,
-					'maxBOF': 0,
-				}],
+				'no-multiple-empty-lines': [
+					'error', {
+						'max': 1,
+						'maxEOF': 0,
+						'maxBOF': 0,
+					},
+				],
 				'strict': ['error', 'global'],
 				'space-before-function-paren': ['error', 'never'],
 				'space-before-blocks': 'error',
@@ -25,9 +25,9 @@ module.exports = {
 				'no-unreachable': 'error',
 				'no-unused-vars': 'error',
 				'array-bracket-spacing': ['error', 'never'],
-				'comma-spacing': ['error', {'before': false, 'after': true}],
-				'no-console': ['error', { 'allow' : ['info', 'warn', 'error'] }],
-				'indent': ['error', 'tab', {'SwitchCase': 1}],
+				'comma-spacing': ['error', { 'before': false, 'after': true }],
+				'no-console': ['error', { 'allow': ['info', 'warn', 'error'] }],
+				'indent': ['error', 'tab', { 'SwitchCase': 1 }],
 				'arrow-spacing': 'error',
 				'react/prop-types': 0,
 				'no-trailing-spaces': 'error',
@@ -35,35 +35,45 @@ module.exports = {
 				'space-infix-ops': ['error'],
 				'one-var-declaration-per-line': ['error'],
 				'comma-dangle': ['error', 'always-multiline'],
-				'keyword-spacing': ['error', {
-					'overrides': {
-						'if': { 'after': false },
-						'switch': { 'after': false },
-						'while': { 'after': false },
-						'for': { 'after': false },
+				'keyword-spacing': [
+					'error', {
+						'overrides': {
+							'if': { 'after': false },
+							'switch': { 'after': false },
+							'while': { 'after': false },
+							'for': { 'after': false },
+						},
 					},
-				}],
+				],
 				'space-in-parens': ['error', 'never'],
 				'function-paren-newline': ['error', 'multiline'],
 				'no-multi-spaces': 'error',
 				'semi-spacing': 'error',
 				'block-spacing': 'error',
-				'no-tabs': ['error', {
-					'allowIndentationTabs': true,
-				}],
-				'padded-blocks': ['error', {
-					'blocks': 'never',
-				}],
+				'no-tabs': [
+					'error', {
+						'allowIndentationTabs': true,
+					},
+				],
+				'padded-blocks': [
+					'error', {
+						'blocks': 'never',
+					},
+				],
 				'array-element-newline': ['error', 'consistent'],
-				'array-bracket-newline': ['error', {
-					'multiline': true,
-				}],
+				'array-bracket-newline': [
+					'error', {
+						'multiline': true,
+					},
+				],
 				'max-statements-per-line': 'error',
-				'max-lines': ['warn', {
-					'max': 500,
-					'skipBlankLines': true,
-					'skipComments': true,
-				}],
+				'max-lines': [
+					'warn', {
+						'max': 500,
+						'skipBlankLines': true,
+						'skipComments': true,
+					},
+				],
 				'no-undef': 'error',
 				'key-spacing': 'error',
 				'padding-line-between-statements': [
@@ -73,7 +83,7 @@ module.exports = {
 				],
 				'object-shorthand': ['error', 'always'],
 				'prefer-const': 'error',
-			}
+			},
 		},
 		'recommended-jsx': {
 			extends: [
@@ -85,65 +95,74 @@ module.exports = {
 				'react',
 			],
 			rules: {
-				'react/jsx-curly-spacing': ['error', {
-					'when': 'always',
-					'children': true,
-				}],
+				'react/jsx-curly-spacing': [
+					'error', {
+						'when': 'always',
+						'children': true,
+					},
+				],
 				'react/jsx-equals-spacing': ['error', 'never'],
-				'react/jsx-tag-spacing': ['error', {
-					'closingSlash': 'never',
-					'beforeSelfClosing': 'always',
-					'afterOpening': 'never',
-					'beforeClosing': 'never',
-				}],
+				'react/jsx-tag-spacing': [
+					'error', {
+						'closingSlash': 'never',
+						'beforeSelfClosing': 'always',
+						'afterOpening': 'never',
+						'beforeClosing': 'never',
+					},
+				],
 				'react/jsx-indent': ['error', 'tab'],
 				'jsx-quotes': ['error', 'prefer-single'],
 				'react/jsx-curly-newline': 'error',
-			}
+				'react/jsx-max-props-per-line': [
+					'error', {
+						maximum: {
+							single: 2,
+							multi: 1,
+						},
+					},
+				],
+				'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
+			},
 		},
 		'recommended-jsx-jk': {
-			extends: [
-				'plugin:jk/recommended-jsx',
-			],
+			extends: ['plugin:jk/recommended-jsx'],
 			plugins: [
 				'jk',
 				'react',
 			],
 			rules: {
 				'jk/jsx-closing-bracket-location': 'error',
-			}
+			},
 		},
 		'default-jsx-node': {
-			"extends": [
-				"plugin:jk/recommended-jsx-jk",
+			'extends': ['plugin:jk/recommended-jsx-jk'],
+			'parser': '@babel/eslint-parser',
+			'plugins': [
+				'jk',
+				'react',
 			],
-			"parser": "@babel/eslint-parser",
-			"plugins": [
-				"jk",
-				"react",
-			],
-			"settings": {
-				"react": {
-					"pragma": "React",
-					"version": "detect"
-				}
-			},
-			"parserOptions": {
-				"ecmaVersion": 2019,
-				"ecmaFeatures": {
-					"jsx": true,
-					"modules": true,
-					"es6": true,
-					"blockBindings": true,
-					"arrowFunctions": true,
-					"objectLiteralShorthandProperties": true,
+			'settings': {
+				'react': {
+					'pragma': 'React',
+					'version': 'detect',
 				},
 			},
-			"env": {
-				"browser": true,
-				"node": true,
-				"es6": true
-			}
-		}
-	}
+			'parserOptions': {
+				'ecmaVersion': 2019,
+				'ecmaFeatures': {
+					'jsx': true,
+					'modules': true,
+					'es6': true,
+					'blockBindings': true,
+					'arrowFunctions': true,
+					'objectLiteralShorthandProperties': true,
+				},
+			},
+			'env': {
+				'browser': true,
+				'node': true,
+				'es6': true,
+			},
+		},
+	},
 };
