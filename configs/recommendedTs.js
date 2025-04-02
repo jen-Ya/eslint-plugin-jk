f47dbd8b5a45ac609e57ae6d217f027c27511dc9
j.kovalev <j.kovalev@q1.eu>
2025-04-02 12:14:22 +0200

    Reorder recommended typescript rules to overwrite defaults

--- configs/recommendedTs.js
+++ configs/recommendedTs.js
@@ -2,7 +2,7 @@ import recommended from './recommended.js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-	recommended,
 	tseslint.configs.recommended,
 	tseslint.configs.stylistic,
+	recommended,
 );
