66c7e887867d8a13b14abac5024669c26dc0fea5
j.kovalev <j.kovalev@q1.eu>
2025-04-02 12:15:50 +0200

    Add global variables from 'globals' package to TypeScript ESLint configuration

--- configs/defaultTs.js
+++ configs/defaultTs.js
@@ -1,12 +1,17 @@
 import recommendedTs from './recommendedTs.js';
 import typescriptParser from '@typescript-eslint/parser';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default tseslint.config(
 	recommendedTs,
 	{
 		languageOptions: {
 			parser: typescriptParser,
+			globals: {
+				...globals.node,
+				...globals.es2025,
+			}
 		},
 	},
 );
