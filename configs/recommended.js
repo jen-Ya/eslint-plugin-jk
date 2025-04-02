0edd8d3df8fea63127442cf15bb5f0d98e6de358
j.kovalev <j.kovalev@q1.eu>
2025-04-02 11:34:05 +0200

    Correct file reference for jsOpinions import in recommended config

--- configs/recommended.js
+++ configs/recommended.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import jsOpinions from '../opinions/js';
+import jsOpinions from '../opinions/js.js';
 
 export default tseslint.config(
 	js.configs.recommended,
